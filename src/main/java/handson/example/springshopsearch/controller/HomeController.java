18b51a5e710ea42ddb70caa81043117bf31b40b4
Unknown <shota_katayama@unirita.co.jp>
2018-10-30 16:37:30 +0900

    課題完成
    オプショナルに悩まされたが解決できた

--- src/main/java/handson/example/springshopsearch/controller/HomeController.java
+++ src/main/java/handson/example/springshopsearch/controller/HomeController.java
@@ -1,11 +1,16 @@
 package handson.example.springshopsearch.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import handson.example.springshopsearch.model.item.Item;
 import handson.example.springshopsearch.model.item.ItemRepository;
 
 @Controller
@@ -16,10 +21,29 @@ public class HomeController {
     ItemRepository itemRepository;
 
     @GetMapping
-    public String index(Model model) {
-        model.addAttribute("items", itemRepository.findAll());
-        return "index";
-    }
+    public String index(
+            Model model,
+            @RequestParam(name = "param",required = false) Optional<String> param,
+            	@RequestParam(name = "keyword", required = false) Optional<String> keyword) {
+    				List<Item> list = itemRepository.findAll();
+    				if(keyword.isPresent()) {
+    					if(param.get().equals("name")) {
+    						list = itemRepository.findByNameContainsOrderByIdAsc(keyword.get());
+    					}
+    					else if(param.get().equals("description")) {
+    						list = itemRepository.findByDescriptionContainsOrderByIdAsc(keyword.get());
+    					}
+    					else if(param.get().equals("nameordescription")) {
+    						list = itemRepository.findByNameOrDescriptionContainsOrderByIdAsc(keyword.get() , keyword.get());
+    					}
+    					
+    				}
+    				else {
+						list = itemRepository.findAll();
+					}
+        			model.addAttribute("items", list);
+        			return "index";
+    		}
 
     @GetMapping("about")
     public String getAbout() {
